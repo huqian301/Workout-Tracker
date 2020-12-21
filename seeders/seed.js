09041e2a774d8086021588c297aa5fc9ce550404
huqian301 <huqian301@gmail.com>
2020-12-20 17:48:21 -0800

    fixing connection"

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,13 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("process.env.MONGODB_URI", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  },  console.log('connect'));
+
 
 let workoutSeed = [
   {
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 
+require('dotenv').config();
+const { connectDB } = require('./config/connection');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,15 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("process.env.MONGODB_URI", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+connectDB();
+
 
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port http://localhost:3000`);
+  console.log(`App running on port ${PORT}!`);
 });
